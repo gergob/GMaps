3676634a9395349c8256fc2a06e351850b84d133
gergob <greg@grelution.com>
2015-08-23 21:08:29 +0200

    added mapTypes

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,7 +1,45 @@
-var map;
+//var map;
 function initializeMap() {
-    map = new google.maps.Map(document.getElementById('mymap'), {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 8
-    });
+    if(google && google.maps && typeof(google.maps.Map) === 'function') {
+
+        //
+        // Roadmap - DEFAULT
+        //
+        var myMapElement = document.getElementById('stdMap');
+        var stdMap = new google.maps.Map(myMapElement, {
+            center: {lat: 42, lng: 0.3},
+            zoom: 6
+        });
+
+
+        //
+        // Satellite Map
+        //
+        var satelliteMapElement = document.getElementById('satelliteMap');
+        var satelliteMap = new google.maps.Map(satelliteMapElement, {
+            center: {lat: 42, lng: 0.3},
+            zoom: 6,
+            mapTypeId: google.maps.MapTypeId.SATELLITE
+        });
+
+        //
+        // Hybrid Map
+        //
+        var hybridMapElement = document.getElementById('hybridMap');
+        var hybridMap = new google.maps.Map(hybridMapElement, {
+            center: {lat: 42, lng: 0.3},
+            zoom: 6,
+            mapTypeId: google.maps.MapTypeId.HYBRID
+        });
+
+        //
+        // Terrain Map
+        //
+        var terrainMapElement = document.getElementById('terrainMap');
+        var terrainMap = new google.maps.Map(terrainMapElement, {
+            center: {lat: 42, lng: 0.3},
+            zoom: 6,
+            mapTypeId: google.maps.MapTypeId.TERRAIN
+        });
+    }
 }
